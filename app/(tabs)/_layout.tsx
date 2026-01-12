f0248b9c4a34c6497075805b27d8f3032abe918b
Manshad Changampalli <manshadchangampalli010@gmail.com>
2026-01-12 22:50:25 +0400

    Refactor TabLayout to include SafeAreaView and update styles in index.tsx for proper layout alignment.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,39 +4,34 @@ import { StyleSheet } from "react-native";
 
 export default function TabLayout() {
     return (
-        // <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
-        <NativeTabs>
-            <NativeTabs.Trigger name="index">
-                <Icon
-                    sf={{ default: "house", selected: "house.fill" }}
-                    drawable="custom_home_drawable"
-                />
-            </NativeTabs.Trigger>
-            <NativeTabs.Trigger name="offers">
-                <Icon
-                    sf={{ default: "star.circle", selected: "star.circle.fill" }}
-                    drawable="custom_offers_drawable"
-                />
-            </NativeTabs.Trigger>
-            <NativeTabs.Trigger name="account">
-                <Icon
-                    sf={{ default: "person", selected: "person.fill" }}
-                    drawable="custom_account_drawable"
-                />
-            </NativeTabs.Trigger>
-            <NativeTabs.Trigger name="cart">
-                <Icon
-                    sf={{ default: "cart", selected: "cart.fill" }}
-                    drawable="custom_cart_drawable"
-                />
-            </NativeTabs.Trigger>
-            <NativeTabs.Trigger
-                name="search"
-                role="search">
-                <Label>Search</Label>
-            </NativeTabs.Trigger>
-        </NativeTabs>
-        // </SafeAreaView>
+        <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
+            <NativeTabs>
+                <NativeTabs.Trigger name="index">
+                    <Icon
+                        sf={{ default: "house", selected: "house.fill" }}
+                        drawable="custom_home_drawable"
+                    />
+                </NativeTabs.Trigger>
+                <NativeTabs.Trigger name="offers">
+                    <Icon
+                        sf={{ default: "star.circle", selected: "star.circle.fill" }}
+                        drawable="custom_offers_drawable"
+                    />
+                </NativeTabs.Trigger>
+                <NativeTabs.Trigger name="account">
+                    <Icon
+                        sf={{ default: "person", selected: "person.fill" }}
+                        drawable="custom_account_drawable"
+                    />
+                </NativeTabs.Trigger>
+                <NativeTabs.Trigger name="cart">
+                    <Icon
+                        sf={{ default: "cart", selected: "cart.fill" }}
+                        drawable="custom_cart_drawable"
+                    />
+                </NativeTabs.Trigger>
+            </NativeTabs>
+        </SafeAreaView>
     );
 }
 
